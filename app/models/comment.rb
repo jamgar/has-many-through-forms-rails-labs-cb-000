9d10a8ef69f1ef43c74c511b955da6e6ddb8b969
James Garcia <jgarcia@garciajames.com>
2017-07-31 22:06:56 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes.all? { |key, value| value.blank? } }
+
+  def reject_comment(attributes)
+    attributes[:content].blank?
+  end
 
 end
